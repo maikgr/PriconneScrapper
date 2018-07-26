58b047e6c6a11fa4618eec306d2f4070da836501
MGibran <maikgibranr@gmail.com>
2018-07-27 00:02:59 +0700

    Add non-author error reply

--- bot.js
+++ bot.js
@@ -29,6 +29,10 @@ client.on('message', (msg) => {
 
     if (!command) return;
 
+    if (command.ownerOnly && msg.author.id !== process.env.OWNER_ID) {
+        return msg.reply(`Sorry, you don't have permission to use this command.`);
+    }
+
     if (command.args && !args.length) {
         let reply = "Incorrect command usage.";
 
